76c05e54f2e6146aa15bc485bdcd6bd6b73d62e5
Stivi7 <stivi.ndoni@gmail.com>
2017-12-06 17:07:06 +0100

    working on front

--- places/static/places/main.js
+++ places/static/places/main.js
@@ -14,15 +14,14 @@ var places = document.getElementById("places")
             type: 'GET',
             dataType: 'json',
             success: build,
-
-
-
         });
     }
 
 
     function build(data) {
        // var json = JSON.parse(data)
+        places.innerHTML = "";
+
         data.forEach(function(obj) {
             var block = createBlock(obj)
             places.appendChild(block)
@@ -30,24 +29,58 @@ var places = document.getElementById("places")
 
     }
 
+    // create imasge element
+    function createImage(obj) {
+        var img = document.createElement('img');
+        img.setAttribute('src', obj.image);
+        img.setAttribute('style', 'width: 80px; height: 80px');
+
+        return img;
+    }
+
+    // create h5 for place's name
+    function createH(obj) {
+        var h5 = document.createElement('h5');
+        h5.innerHTML = obj.name;
+        return h5;
+    }
+
+    //location address
+    function createAddress(obj) {
+        var address = document.createElement('address');
+        address.innerHTML ="Address: " + obj.city
+            + obj.location + "<br/>"
+            + "Phone: " + obj.phone;
+        return address;
+    }
+
+    //create button for 'going' 'not going'
+    function createButton(obj) {
+        var btn = document.createElement('button')
+        btn.setAttribute('class', 'btn btn-default btn-outline-primary');
+        btn.setAttribute('id', obj.id);
+        btn.innerHTML = '0';
+        return btn;
+    }
 
     //create the div with needed info
     function createBlock(obj) {
         var div = document.createElement('div');
-        var img = document.createElement('img');
-        var h5 = document.createElement('h5');
-        var address = document.createElement('address');
-
-        // div.setAttribute('style', 'width: 30%');
-        img.setAttribute('src', obj.image);
-        img.setAttribute('style', 'width: 80px; height: 80px')
-        address.innerHTML ="Address: " + obj.city
-            + obj.location + "<br/>"
-            + "Phone: " + obj.phone;
-        h5.innerHTML = obj.name;
-        div.appendChild(h5);
+        var span = document.createElement('span');
+        var divrow = document.createElement('div');
+        var img = createImage(obj);
+        var h5 = createH(obj);
+        var address = createAddress(obj);
+        var button = createButton(obj);
+        span.innerHTML = 'Going';
+        divrow.setAttribute('class', 'row');
+        divrow.appendChild(h5);
+        divrow.appendChild(span);
+        divrow.appendChild(button);
+        div.appendChild(divrow);
         div.appendChild(img);
-        div.appendChild(address)
+        div.appendChild(address);
+
 
         return div;
     }
